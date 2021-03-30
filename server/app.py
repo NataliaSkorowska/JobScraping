bc869d31c3b016b88265b60ce353a5cb7c563748
Aleksandra Okrój <aleksandra.okroj1@gmail.com>
2021-03-30 11:35:46 +0200

    Scraping offer titles with pagination

--- server/app.py
+++ server/app.py
@@ -13,27 +13,28 @@ app.config.from_object(__name__)
 
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
+# setting up variables that will hold informations about the offers
+offer_title = []
+
+def pracujPlScraping(webpage, page_number):
+    next_page = webpage + str(page_number)
+    response= requests.get(str(next_page))
+    result = BeautifulSoup(response.content, "html.parser")
+    soup_title= result.findAll('a', class_='offer-details__title-link')
+    for x in range(len(soup_title)):
+        offer_title.append(soup_title[x].text)
+        if page_number < 5:
+            page_number = page_number + 1
+            pracujPlScraping(webpage, page_number)
 
 @app.route('/', methods=['GET'])
 def getJob():
     return jsonify({
             'status': 'success',
-            'job_titles': job_titles
+            'job_titles': offer_title
     })
 
-
 if __name__ == '__main__':
-    job_titles=[]
-    #retrieve the HTML data
-    URL = 'https://www.pracuj.pl/praca'
-    page = requests.get(URL)    
-
-    results = BeautifulSoup(page.content, 'html.parser')
-    job_elems = results.find_all('a', class_='offer-details__title-link')
-    for job_elem in job_elems:
-        job_titles.append(job_elem.text)
-        # print(job_elem.text, end='\n'*2)
-    print(job_titles)
-
+    pracujPlScraping('https://www.pracuj.pl/praca?pn=',1)
     app.run()
     
